1760f5293b22ad3ffc5efae965b6ebeacacb96dd
Wanzi <1797441795@qq.com>
2025-08-12 13:52:50 +0800

    Manual fix

--- config/database.ts
+++ config/database.ts
@@ -2,7 +2,7 @@ import path from 'path';
 
 export default ({ env }) => ({
   connection: {
-    client: 'mongoose',
+    client: 'mongo',
     uri: env('DATABASE_URI'), // 使用完整的连接字符串
     options: {
       ssl: env.bool('DATABASE_SSL', true), // Atlas 通常需要 SSL
